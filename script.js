9da65c6123f28d24eabc85d23b279fc98c114c46
Pishtar Salihu <pishtar.salihu@gmail.com>
2023-10-05 13:06:05 +0200

    Add getComputerChoice function

---
+++ script.js
@@ -0,0 +1,9 @@
+// Step 1: Create a function to get a random computer choice.
+function getComputerChoice() {
+  const choices = ["Rock", "Paper", "Scissors"];
+  const randomIndex = Math.floor(Math.random() * 3);
+  return choices[randomIndex];
+}
+
+
+
