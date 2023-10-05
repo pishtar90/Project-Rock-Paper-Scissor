4d4eda065d299b609b43ca38baf23041c65ebdb5
Pishtar Salihu <pishtar.salihu@gmail.com>
2023-10-05 13:18:17 +0200

    Add playRound function

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ function getComputerChoice() {
 const computerChoice = getComputerChoice();
 console.log("Computer's Choice:", computerChoice);
 
+// Step 2: Write a function that plays a single round of Rock Paper Scissors.
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+
+  if (playerSelection === computerSelection) {
+    return "It's a tie!";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "rock")
+  ) {
+    return `You win! ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return `You lose! ${computerSelection} beats ${playerSelection}`;
+  }
+}
